d508d494a027c21cff1ded13a82c42da22a56133
kylinsoong <kylinsoong.1214@gmail.com>
2016-06-06 13:55:53 +0800

    TEIID-3398: add initial commit

---
+++ utils/src/main/java/org/teiid/sizing/TestTmp.java
@@ -0,0 +1,12 @@
+package org.teiid.sizing;
+
+//TODO-- need remove 
+public class TestTmp {
+
+    public static void main(String[] args) {
+
+        args = new String[]{"serialization", "122"};
+        TeiidUtils.main(args);
+    }
+
+}
